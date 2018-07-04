52a6b94c4b618c893476ea873d000898a5c6d016
matyX6 <matyX6@gmail.com>
2018-07-04 18:42:13 +0200

    AA4 - ship,music,load,splash,waypoint, quiz1

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start ()
+    {
+        Invoke("LoadNextLevel", 3f);	
+	}
+
+    void LoadNextLevel()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}

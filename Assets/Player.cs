755db6a7c459794fe0e995b0800a22b9bcc43f48
matyX6 <matyX6@gmail.com>
2018-07-05 12:40:37 +0200

    AA5 - added very basic player controls

---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s")][SerializeField] float Speed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 4.5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerShipControls();
+    }
+
+    private void PlayerShipControls()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float yOffset = yThrow * Speed * Time.deltaTime;
+
+        float rawXPosition = transform.localPosition.x + xOffset;
+        float rawYPosition = transform.localPosition.y + yOffset;
+
+        float clampedXPosition = Mathf.Clamp(rawXPosition, -xRange, xRange);
+        float clampedYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPosition, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPosition, transform.localPosition.z);
+    }
+}

21539f3ad77f236c195787513309bf30bf07b730
matyX6 <matyX6@gmail.com>
2018-07-07 17:15:01 +0200

    AA12 - enable/disable controls on death, player components split

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In m/s")][SerializeField] float controlSpeed = 10f;
+    [Tooltip("In m")] [SerializeField] float xRange = 4.5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+    [Header("Screen-Position Based")]
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 5f;
+
+    [Header("Control-Throe Based")]
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+
+    private void OnPlayerDeath() //called by string reference
+    {
+        isControlEnabled = false;
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float controlDueToPosition = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + controlDueToPosition;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+
+        float rawXPosition = transform.localPosition.x + xOffset;
+        float rawYPosition = transform.localPosition.y + yOffset;
+
+        float clampedXPosition = Mathf.Clamp(rawXPosition, -xRange, xRange);
+        float clampedYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPosition, clampedYPosition, transform.localPosition.z);
+    }
+}

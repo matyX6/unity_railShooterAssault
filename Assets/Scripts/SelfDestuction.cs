544417f9b5fd75ae2106b6ac179259e75da3ee75
matyX6 <matyX6@gmail.com>
2018-07-09 12:45:40 +0200

    AA15 - enemy script, self destruction script, minor changes

---
+++ Assets/Scripts/SelfDestuction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestuction : MonoBehaviour {
+
+    [Tooltip("In seconds")][SerializeField] float destroyTimer = 3f;
+
+	void Start ()
+    {
+        Destroy(gameObject, destroyTimer);
+	}
+}
